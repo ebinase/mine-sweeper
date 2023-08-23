44e6cf6a128587423afa330ef6a501ef5c4c51dc
ebina <ebina.engr@gmail.com>
2023-08-24 01:09:15 +0900

    デフォルト動作をキャンセル

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -65,8 +65,8 @@ const Cell: React.FC<Props> = ({ cell, handleClick, isFailed = false }) => {
       }
       onMouseDown={(e) => e.button !== RIGHT_CLICK_EVENT && handleMouseDown()} // 右クリックで開放してしまうのを防ぐ
       onMouseUp={(e) => e.button !== RIGHT_CLICK_EVENT && handleMouseUp()}
-      onTouchStart={handleMouseDown}
-      onTouchEnd={handleMouseUp}
+      onTouchStart={(e) => {e.preventDefault(); handleMouseDown()}}
+      onTouchEnd={(e) => {e.preventDefault(); handleMouseUp()}}
       onContextMenu={(e) => {
         e.preventDefault();
         setIsFlagged(!isFlagged);
