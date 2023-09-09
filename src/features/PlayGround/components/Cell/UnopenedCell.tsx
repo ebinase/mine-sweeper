8db2f2c6ffea4b666f4dc874f78aa9be4b845c36
ebina <ebina.engr@gmail.com>
2023-09-10 02:03:48 +0900

    フラグとハテナを切り替えられるようにした

--- src/features/PlayGround/components/Cell/UnopenedCell.tsx
+++ src/features/PlayGround/components/Cell/UnopenedCell.tsx
@@ -3,7 +3,7 @@
 import useLongPress from '@/hooks/useLongPress';
 import Image from 'next/image';
 import { CellData } from '../../hooks/useBoard';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 type Props = {
   handleClick: (id: number) => void;
@@ -12,9 +12,21 @@ type Props = {
 };
 
 const UnopenedCell: React.FC<Props> = ({ handleClick, cell, toggleFlag }) => {
-  // マスの開放、長押しでフラグでのフラグの切り替え
+  // フラグをハテナ表示に切り替えできるようにする
+  const [isSuspicious, setIsSuspicious] = useState(false);
+  /**
+   * フラグなし
+   * - 左クリック: 開放
+   * - 右クリック: フラグを立てる
+   * フラグあり
+   * - 左クリック: フラグ/ハテナを切り替え
+   * - 左クリック長押し: フラグを外す
+   * - 右クリック: フラグを外す
+   */
   const handleLongPress = () => toggleFlag(cell.id);
-  const handleClickWithFlag = () => (cell.isFlagged ? toggleFlag(cell.id) : handleClick(cell.id)); // フラグが立っているときは開放しない
+  const toggleFlagAndQuestion = () => setIsSuspicious((prev) => !prev);
+  const handleClickWithFlag = () =>
+    cell.isFlagged ? toggleFlagAndQuestion() : handleClick(cell.id); // フラグが立っているときは開放しない
   const longPressEvent = useLongPress(handleLongPress, handleClickWithFlag);
   // 右クリックでフラグを切り替える
   const handleContextMenu = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
@@ -27,15 +39,20 @@ const UnopenedCell: React.FC<Props> = ({ handleClick, cell, toggleFlag }) => {
       {...longPressEvent}
       onContextMenu={handleContextMenu}
     >
-      {cell.isFlagged && (
-        <Image
-          src='/flag.png'
-          alt='red flag'
-          width={30}
-          height={30}
-          className={'w-3/5 lg:animate-none animate-slide-in-blurred-top pointer-events-none'}
-        />
-      )}
+      {cell.isFlagged &&
+        (isSuspicious ? (
+          <span className='text-gray-300 md:text-2xl' >
+            ?
+          </span>
+        ) : (
+          <Image
+            src='/flag.png'
+            alt='red flag'
+            width={30}
+            height={30}
+            className={'w-3/5 lg:animate-none animate-slide-in-blurred-top pointer-events-none'}
+          />
+        ))}
     </div>
   );
 };
