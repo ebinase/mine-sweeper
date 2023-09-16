054e42601453aae7dec0fd0ebf610e4c332b7173
ebina <ebina.engr@gmail.com>
2023-09-16 23:47:08 +0900

    隠し機能的にフラグとハテナを切り替えられるようにした

--- src/features/PlayGround/components/Header/index.tsx
+++ src/features/PlayGround/components/Header/index.tsx
@@ -2,27 +2,37 @@
 
 import Image from 'next/image';
 import { BoardConfig } from '../../functions/board';
+import { useState } from 'react';
 
 type Props = {
-    normalFlags: number;
-    suspectedFlags: number;
-    boardConfig: BoardConfig;
+  normalFlags: number;
+  suspectedFlags: number;
+  boardConfig: BoardConfig;
 };
 
-export const Header: React.FC<Props> = ({normalFlags, suspectedFlags, boardConfig}) => {
-    return (
-        <header className='flex justify-between items-center py-0.5'>
-        <h1>Mine Sweeper</h1>
-        <div className='flex gap-2'>
+export const Header: React.FC<Props> = ({ normalFlags, suspectedFlags, boardConfig }) => {
+  const [showNormalFlag, setShowNormalFlag] = useState(true);
+  return (
+    <header className='flex justify-between items-center py-0.5'>
+      <h1>Mine Sweeper</h1>
+      <div className='flex gap-2' onClick={() => setShowNormalFlag(!showNormalFlag)}>
+        {showNormalFlag ? (
           <div className='flex items-center'>
             <Image src='/flag.png' alt='flag' width={15} height={15} />
             <span className='text-xs'>×{normalFlags}</span>
           </div>
+        ) : (
           <div className='flex items-center'>
-            <Image src='/mine.svg' alt='exploded mine' width={15} height={15} />
-            <span className='text-xs'>×{boardConfig.mines}</span>
+            <span className='text-gray-600 dark:invert'>?</span>
+            <span className='text-xs'>×{suspectedFlags}</span>
           </div>
+        )}
+
+        <div className='flex items-center'>
+          <Image src='/mine.svg' alt='exploded mine' width={15} height={15} />
+          <span className='text-xs'>×{boardConfig.mines}</span>
         </div>
-      </header>
-    );
+      </div>
+    </header>
+  );
 };
